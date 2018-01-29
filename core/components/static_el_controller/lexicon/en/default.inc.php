6f66c49c81691e9a299bf0d0695c9bbb7d08edb0
Andrew Haupt <ajhaupt7@gmail.com>
2018-01-29 14:03:40 -0800

    Update lexicon descriptions to be more specific

--- core/components/static_el_controller/lexicon/en/default.inc.php
+++ core/components/static_el_controller/lexicon/en/default.inc.php
@@ -16,13 +16,14 @@ $_lang['setting_static_el_controller.include_category'] = 'Include category fold
 $_lang['setting_static_el_controller.include_id'] = 'Include ID';
 $_lang['setting_static_el_controller.base_element_path'] = 'Base Static Elements Path';
 
-$_lang['setting_static_el_controller.enable_rewrite_desc'] = 'If you change the name of the chunk, snippet, etc. will change the name of a static file. The old file is not deleted, it will be continue to be in the file system.';
-$_lang['setting_static_el_controller.chunk_file_extension_desc'] = 'Set up an extension of chunk static files.';
-$_lang['setting_static_el_controller.template_file_extension_desc'] = 'Set up an extension of template static files.';
-$_lang['setting_static_el_controller.snippet_file_extension_desc'] = 'Set up an extension of snippet static files.';
-$_lang['setting_static_el_controller.plugin_file_extension_desc'] = 'Set up an extension of plugin static files.';
+$_lang['setting_static_el_controller.enable_rewrite_desc'] = 'If "Yes", changing the name of the element will change the name of a static file. The old file is not deleted, it will be continue to be in the file system';
+$_lang['setting_static_el_controller.chunk_file_extension_desc'] = 'Set up an extension for chunk static files.';
+$_lang['setting_static_el_controller.template_file_extension_desc'] = 'Set up an extension for template static files.';
+$_lang['setting_static_el_controller.snippet_file_extension_desc'] = 'Set up an extension for snippet static files.';
+$_lang['setting_static_el_controller.plugin_file_extension_desc'] = 'Set up an extension for plugin static files.';
 $_lang['setting_static_el_controller.element_media_source_desc'] = 'ID of Static Element Media Source';
-$_lang['setting_static_el_controller.static_by_default_desc'] = 'If "Yes" sets all elements static in elements\' forms';
-$_lang['setting_static_el_controller.include_category_desc'] = 'If "Yes" file is placed to folder with the name of category.';
-$_lang['setting_static_el_controller.include_id_desc'] = 'If "Yes" file name is prepended with its ID.';
+$_lang['setting_static_el_controller.static_by_default_desc'] = 'If "Yes", sets all elements static in elements\' forms';
+$_lang['setting_static_el_controller.include_category_desc'] = 'If "Yes", category name will be added to the element\'s path.';
+$_lang['setting_static_el_controller.include_id_desc'] = 'If "Yes", element\'s file name is prepended with its ID.';
+$_lang['setting_static_el_controller.base_element_path_desc'] = 'Base path for all static elements within the filesystem';
 
